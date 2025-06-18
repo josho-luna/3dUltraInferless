d25f3c761bfb87a00dd88be4a6b90c958f97f4ab
Josue Luna <j.luna2016@hotmail.com>
2025-06-18 09:51:32 -0600

    help 2

--- app.py
+++ app.py
@@ -54,33 +54,35 @@ class InferlessPythonModel:
     @app.infer
     def infer(self, inputs):
 
-        try:
-            # Load Image and turn it into PIL image
-            img = inputs["image_bytes"]
+        
+        # Load Image and turn it into PIL image
+        img = inputs["image_bytes"]
 
-            img = load_image(img).resize((1024, 1024), Image.LANCZOS)
-            control_image = self.preprocess_img(img)
+        img = load_image(img).resize((1024, 1024), Image.LANCZOS)
+        control_image = self.preprocess_img(img)
 
-            prompts = [
-                "Photorealistic portrait of a bald cute asleep newborn baby, closed eyes, soft light, DSLR, 85mm lens",
-            #   "Beautiful bald asleep newborn baby in a cradle, wearing a soft blue baby cap, warm diffuse lighting, natural tones",
-                "Peaceful sleeping newborn baby, bald, close-up with detailed skin texture, photorealistic, pink skin, shallow depth of field",
-                "Portrait of a sleeping newborn baby with closed eyes, resting peacefully in a soft womb-like environment, warm tones, detailed baby hands"
-            ]
+        prompts = [
+            "Photorealistic portrait of a bald cute asleep newborn baby, closed eyes, soft light, DSLR, 85mm lens",
+        #   "Beautiful bald asleep newborn baby in a cradle, wearing a soft blue baby cap, warm diffuse lighting, natural tones",
+            "Peaceful sleeping newborn baby, bald, close-up with detailed skin texture, photorealistic, pink skin, shallow depth of field",
+            "Portrait of a sleeping newborn baby with closed eyes, resting peacefully in a soft womb-like environment, warm tones, detailed baby hands"
+        ]
 
-            negative_prompt = "hair, deformed, fingers, sad, ugly, disgusting, uncanny, blurry, grainy, monochrome, duplicate, artifact, watermark, text"
+        negative_prompt = "hair, deformed, fingers, sad, ugly, disgusting, uncanny, blurry, grainy, monochrome, duplicate, artifact, watermark, text"
 
-            num_inference_steps = [30, 25, 45]
-            controlnet_conditioning_scale = [0.6, 0.75, 0.9]
-            guidance = [7.0, 10.0, 12.0]
+        num_inference_steps = [30, 25, 45]
+        controlnet_conditioning_scale = [0.6, 0.75, 0.9]
+        guidance = [7.0, 10.0, 12.0]
 
-            # Optional: set different generators for reproducibility
-            seeds = [43, 44, 45]
-            generators = [torch.manual_seed(seed) for seed in seeds]
+        # Optional: set different generators for reproducibility
+        seeds = [43, 44, 45]
+        generators = [torch.manual_seed(seed) for seed in seeds]
 
-            # Run batch if your pipeline supports it
-            with torch.inference_mode():
-                output_images = self.pipeline(
+        # Run batch if your pipeline supports it
+        output_images = []
+        with torch.inference_mode():
+            for i in range(len(prompts)):
+                tmp = self.pipeline(
                     image=[img] * 3,
                     prompt=prompts,
                     negative_prompt=[negative_prompt] * 3,
@@ -92,14 +94,15 @@ class InferlessPythonModel:
                     height=1024,
                     width=1024
                 ).images
+                output_images.append(tmp)
+                
+            
+                
 
-            # Convert to base64
-            output_imgs = [self.encode_base64(image) for image in output_images]
-            return {"images": output_imgs}
-        except Exception as e:
-            print(e)
-            return []
-        
+        # Convert to base64
+        output_imgs = [self.encode_base64(image) for image in output_images]
+        return {"images": output_imgs}
+
 
 
 
