bc1481abb9400ba68d0b22ef343c30daf1b0edc9
Yahir Luna <j.luna2016@hotmail.com>
2025-06-19 16:39:49 -0600

    I think it works now (v4)

--- input_schema.py
+++ input_schema.py
@@ -5,7 +5,7 @@ INPUT_SCHEMA = {
         'shape': [1],
         'example': ["https://raw.githubusercontent.com/josho201/3dUltra/refs/heads/main/imgs/test2.png"]
     },
-    'num_inference_steps': {
+    'inference_steps': {
         'datatype': 'INT8',
         'required': True,
         'shape': [1],
